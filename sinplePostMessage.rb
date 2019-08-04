e6862d2b1eef57589301f629deebaf70fc6eef29
tettsu <town-field@live.jp>
2019-08-04 23:25:19 +0900

    シンプルにメッセージを送るだけのrbを追加してみる

---
+++ sinplePostMessage.rb
@@ -0,0 +1,14 @@
+require 'slack-ruby-client'
+require 'inifile'
+
+Slack.configure do |config|
+  @ini = IniFile.load("secret.conf")
+  val = @ini['secret']['token']  
+  config.token = val
+end
+
+@ini = IniFile.load("secret.conf")
+channelName = @ini['channel']['name']
+
+client = Slack::Web::Client.new
+client.chat_postMessage(channel: channelName, text: あああ')
