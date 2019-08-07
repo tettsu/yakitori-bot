457e0a5e43460b2b6087ba3f5bf28181265d69cf
tettsu <town-field@live.jp>
2019-08-07 19:56:53 +0900

    開発チャンネルと処理をわけた

--- main.rb
+++ main.rb
@@ -32,8 +32,8 @@ require './praiseWords'
 
   # ユーザからのメッセージを検知したときの処理
   client.on :message do |data|
-    # 開発チャンネルまたは称賛チャンネルの共通の処理
-    if data['channel'] == devChId || data['channel'] == praiseCh
+    # 開発チャンネルの処理
+    if data['channel'] == devChId
       if data['text'].include?('こんにちは')
         client.message channel: devChId, text: "こんにちは。"
       end
@@ -73,6 +73,39 @@ require './praiseWords'
     if data['channel'] == praiseCh
       if data['text'].include?('ほめて') || data['text'].include?('褒めて')
         client.message channel: praiseCh, text: getRandomPraiseWord
+      end
+      if data['text'].include?('こんにちは')
+        client.message channel: praiseCh, text: "こんにちは。"
+      end
+      if data['text'].include?('おはよう')
+        client.message channel: praiseCh, text: "おはよう。"
+      end
+      if data['text'].include?('こんばん')
+        client.message channel: praiseCh, text: "こんばんわ。"
+      end
+      if data['text'].include?('おやすみ')
+        client.message channel: praiseCh, text: "おやすみなさい。"
+      end
+      if data['text'].include?('ムーミンパパ')
+        client.message channel: praiseCh, text: "https://www.moomin.co.jp/characters/moominpappa"
+      end
+      if data['text'].include?('ムーミンママ')
+        client.message channel: praiseCh, text: "https://www.moomin.co.jp/characters/moominmamma"
+      end
+      if data['text'].include?('スナフキン')
+        client.message channel: praiseCh, text: "https://www.moomin.co.jp/characters/snufkin"
+      end
+      if data['text'].include?('スニフ')
+        client.message channel: praiseCh, text: "https://www.moomin.co.jp/characters/sniff"
+      end
+      if data['text'].include?('ミィ') || data['text'].include?('ミイ')
+        client.message channel: praiseCh, text: "https://www.moomin.co.jp/characters/little-my"
+      end
+      if data['text'].include?('おじょうさん') || data['text'].include?('お嬢さん') || data['text'].include?('ノンノン') || data['text'].include?('フローレン')
+        client.message channel: praiseCh, text: "https://www.moomin.co.jp/characters/snorkmaiden"
+      end
+      if data['text'].include?('ムーミン')
+        client.message channel: praiseCh, text: "https://www.moomin.co.jp/characters/moomintroll"
       end
     end
     
